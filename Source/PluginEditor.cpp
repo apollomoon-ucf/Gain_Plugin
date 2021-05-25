8453ab8f32720df30e42bdaaea27000d7e1de659
brian_moon <apollomoon@knights.ucf.edu>
2021-05-25 09:49:29 -0400

    added dB text under fader, next to numerical gain value

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -14,8 +14,9 @@ Gain_slider_brianMoonAudioProcessorEditor::Gain_slider_brianMoonAudioProcessorEd
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
     gain_slider.setSliderStyle (juce::Slider::SliderStyle::LinearVertical);
-    gain_slider.setTextBoxStyle (juce::Slider::TextBoxBelow, true, 50, 20);
+    gain_slider.setTextBoxStyle (juce::Slider::TextBoxBelow, true, 60, 20);
     gain_slider.setTextBoxIsEditable(true);
+    gain_slider.setTextValueSuffix(" dB");
     gain_slider.setRange (-75.0f, 12.0f, 0.01f);
     gain_slider.setValue (0.0f);
     gain_slider.addListener(this); // this refers to this class
