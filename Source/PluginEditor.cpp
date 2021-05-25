5d3cbaed7082de2d588831951afc0124719c81b0
brian_moon <apollomoon@knights.ucf.edu>
2021-05-24 22:23:01 -0400

    added purple background

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -30,15 +30,17 @@ Gain_slider_brianMoonAudioProcessorEditor::~Gain_slider_brianMoonAudioProcessorE
 void Gain_slider_brianMoonAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    g.fillAll(juce::Colours::rebeccapurple);
+//    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello, World! Brian's first audio plugin!", getLocalBounds(), juce::Justification::centred, 1);
+//    g.setColour (juce::Colours::black);
+//    g.setFont (15.0f);
+//    g.drawFittedText ("Hello, World! Brian's first audio plugin!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void Gain_slider_brianMoonAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+//    gain_slider.setBounds(getWidth() / 2 - 50, getHeight() / 2 - 100, 100, 200);
 }
